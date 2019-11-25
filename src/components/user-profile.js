416cfa49238d28fb6373dc50b761cb8173b0c158
k5va <k5va@yandex.ru>
2019-11-25 09:44:36 +0300

    Refactored page elements to components

---
+++ src/components/user-profile.js
@@ -0,0 +1,10 @@
+/**
+ * Creates user profile template
+ * @return {String} template
+ */
+export const createUserProfileTemplate = () => {
+  return `<section class="header__profile profile">
+    <p class="profile__rating">Movie Buff</p>
+    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+  </section>`;
+};
