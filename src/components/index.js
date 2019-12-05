2dc22d12cc47f6e47bc1ee45fb23fb1fe3586e09
k5va <k5va@yandex.ru>
2019-12-05 18:07:37 +0300

    Added index to reexport components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as FilmComponent} from "./film-card";
+export {default as FilmDetails} from "./film-details";
+export {default as FilmListComponent} from "./films";
+export {default as FooterStaticticsComponent} from "./footer-statistics";
+export {default as MenuComponent} from "./menu";
+export {default as ShowMoreComponent} from "./show-more";
+export {default as SortComponent} from "./sort";
+export {default as UserProfileComponent} from "./user-profile";
