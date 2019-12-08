81db6401c7f072c8742107af6769bf40a57b3fc7
k5va <k5va@yandex.ru>
2019-12-08 10:43:14 +0300

    Extracted template from most-commented-films component

---
+++ src/components/most-commented-films/template.js
@@ -0,0 +1,13 @@
+/**
+ * Creates top rated films template
+ * @return {String} template
+ */
+const template = () => {
+  return `<section class="films-list--extra films-list--most-commented">
+            <h2 class="films-list__title">Most commented</h2>
+            <div class="films-list__container">
+            </div>
+          </section>`;
+};
+
+export {template};
