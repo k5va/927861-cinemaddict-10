3892c3871a3c5d1023de1829bfa54864f9b885e1
k5va <k5va@yandex.ru>
2019-12-08 22:38:29 +0300

    Added films section to no-films template

--- src/components/no-films/template.js
+++ src/components/no-films/template.js
@@ -3,8 +3,10 @@
  * @return {String} template
  */
 const template = () => {
-  return `<section class="films-list">
-            <h2 class="films-list__title">There are no movies in our database</h2>
+  return `<section class="films">
+            <section class="films-list">
+              <h2 class="films-list__title">There are no movies in our database</h2>
+            </section>
           </section>`;
 };
 
