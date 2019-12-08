2660139096cbd236de7c367a2d34713281a40e6c
k5va <k5va@yandex.ru>
2019-12-08 22:38:46 +0300

    Added films section to films template

--- src/components/films/template.js
+++ src/components/films/template.js
@@ -3,10 +3,12 @@
  * @return {String} template
  */
 const template = () => {
-  return `<section class="films-list">
-            <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-            <div class="films-list__container">
-            </div>
+  return `<section class="films">
+            <section class="films-list">
+              <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+              <div class="films-list__container">
+              </div>
+            </section>
           </section>`;
 };
 
