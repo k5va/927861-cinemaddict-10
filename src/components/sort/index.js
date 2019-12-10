d7d0c55cad7fef0df654bc0a1100ea0dc4b40d52
k5va <k5va@yandex.ru>
2019-12-10 16:00:27 +0300

    Added selected sort link toggle

--- src/components/sort/index.js
+++ src/components/sort/index.js
@@ -6,7 +6,7 @@ export default class Sort extends AbstractComponent {
   constructor() {
     super();
 
-    this._currenSortType = SortType.DEFAULT;
+    this._currentSortType = SortType.DEFAULT;
   }
 
   /**
@@ -25,18 +25,41 @@ export default class Sort extends AbstractComponent {
     this.getElement().addEventListener(`click`, (evt) => {
       evt.preventDefault();
 
+      // check if target is a sort link
       if (evt.target.tagName !== `A`) {
         return;
       }
 
       const sortType = evt.target.dataset.sortType;
-      if (this._currenSortType === sortType) {
+      if (this._currentSortType === sortType) {
         // return if sort type hasn't changed
         return;
       }
 
-      this._currenSortType = sortType;
-      handler(this._currenSortType);
+      // toggle sort buttons
+      this._toggleSortLinks(evt.target);
+
+      // save current sort and call handler
+      this._currentSortType = sortType;
+      handler(this._currentSortType);
     });
+  }
+
+  /**
+   * Returns active sort link
+   * @return {HTMLElement} - current active sort link
+   */
+  _getActiveSortLink() {
+    return this.getElement().querySelector(`.sort__button--active`);
+  }
+
+  /**
+   * Changes active sort link to selected
+   * @param {HTMLElement} selectedSortLink - selected sort link
+   */
+  _toggleSortLinks(selectedSortLink) {
+    // toggle sort buttons
+    this._getActiveSortLink().classList.toggle(`sort__button--active`);
+    selectedSortLink.classList.toggle(`sort__button--active`);
   }
 }
