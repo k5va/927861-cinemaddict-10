00d37e3047720732feb22ce9af9738d3def674e5
k5va <k5va@yandex.ru>
2019-12-20 13:53:56 +0300

    Refactores main to use films model

--- src/main.js
+++ src/main.js
@@ -2,23 +2,24 @@ import {UserProfileComponent, MenuComponent, FooterStaticticsComponent} from "./
 import {generateFilms} from "./mock/film";
 import {render} from "./utils";
 import {PageController} from "./controllers";
+import {FilmsModel} from "./models";
 
 const FILMS_COUNT = 14;
-
-const films = generateFilms(FILMS_COUNT);
+const filmsModel = new FilmsModel();
+filmsModel.setFilms(generateFilms(FILMS_COUNT));
 
 // render user profile
 const headerElement = document.querySelector(`.header`);
-render(headerElement, new UserProfileComponent(films));
+render(headerElement, new UserProfileComponent(filmsModel.getFilmsAll()));
 
 // render menu
 const mainElement = document.querySelector(`.main`);
-render(mainElement, new MenuComponent(films));
+render(mainElement, new MenuComponent(filmsModel.getFilmsAll()));
 
 // render films
-const pageController = new PageController(mainElement);
-pageController.render(films);
+const pageController = new PageController(mainElement, filmsModel);
+pageController.render();
 
 // render footer statistics
 const footerElement = document.querySelector(`.footer`);
-render(footerElement, new FooterStaticticsComponent(films.length));
+render(footerElement, new FooterStaticticsComponent(filmsModel.getFilmsAll().length));
