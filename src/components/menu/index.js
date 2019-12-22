e36bcca8a1bd1017bfe5a763ea07d1b05ddb5356
k5va <k5va@yandex.ru>
2019-12-22 20:11:03 +0300

    Dynamically generated menu in menu component

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -2,10 +2,11 @@ import AbstractComponent from "../component";
 import {template} from "./template";
 
 export default class Menu extends AbstractComponent {
-  constructor(films) {
+  constructor(menuItems) {
     super();
 
-    this._films = films;
+    this._menuItems = menuItems;
+    this._currentMenuItemElement = this._findSelectedMenuItem();
   }
 
   /**
@@ -13,6 +14,38 @@ export default class Menu extends AbstractComponent {
    * @return {String} - template
    */
   getTemplate() {
-    return template(this._films);
+    return template(this._menuItems);
+  }
+
+  /**
+   * Sets select menu item handler
+   * @param {Function} handler - handler
+   */
+  setSelectMenuItemHandler(handler) {
+    this
+      .getElement()
+      .addEventListener(`click`, (evt) => {
+        evt.preventDefault();
+
+        // check if target is menu link and it is not already selected
+        if (!evt.target.classList.contains(`main-navigation__item`) ||
+          evt.target.classList.contains(`main-navigation__item--active`)) {
+          return;
+        }
+        // activate selected menu item
+        this._currentMenuItemElement.classList.toggle(`main-navigation__item--active`);
+        evt.target.classList.toggle(`main-navigation__item--active`);
+        this._currentMenuItemElement = evt.target;
+        // call handler
+        handler(evt.target.dataset.menuItem);
+      });
+  }
+
+  /**
+   * Find selected menu item element
+   * @return {HTMLElement} - selected menu item DOM element
+   */
+  _findSelectedMenuItem() {
+    return this.getElement().querySelector(`.main-navigation__item--active`);
   }
 }
