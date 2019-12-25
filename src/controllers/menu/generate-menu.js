aa7df25f16a2001503ddaf152a5acf1e1e3c1fc7
k5va <k5va@yandex.ru>
2019-12-25 13:47:35 +0300

    Refactored   generateMenu

--- src/controllers/menu/generate-menu.js
+++ src/controllers/menu/generate-menu.js
@@ -7,14 +7,14 @@ import {FilterFilms} from "../../utils";
  * @param {String} selectedMenuItem - selected menu item
  * @return {Array<*>} - array of menu items
  */
-const generateMenu = (films, selectedMenuItem) => Object.keys(MenuItem)
-  .map((key) => ({
-    name: MenuItem[key],
-    text: MenuItemText[MenuItem[key]],
-    count: FilterFilms[MenuItem[key]] ? FilterFilms[MenuItem[key]](films).length : null,
-    isCountHidden: MenuItem[key] === MenuItem.ALL,
-    isSelected: MenuItem[key] === selectedMenuItem,
-    isAdditional: MenuItem[key] === MenuItem.STAT
+const generateMenu = (films, selectedMenuItem) => Object.values(MenuItem)
+  .map((menuItem) => ({
+    name: menuItem,
+    text: MenuItemText[menuItem],
+    count: FilterFilms[menuItem] ? FilterFilms[menuItem](films).length : null,
+    isCountHidden: menuItem === MenuItem.ALL,
+    isSelected: menuItem === selectedMenuItem,
+    isAdditional: menuItem === MenuItem.STAT
   }));
 
 export {generateMenu};
