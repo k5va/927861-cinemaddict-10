8f6f53a733dbc43fc07a4e913f24c3e4562fa9ec
k5va <k5va@yandex.ru>
2019-12-28 20:15:38 +0300

    Added watchingDate to mocks

--- src/mock/film.js
+++ src/mock/film.js
@@ -112,7 +112,7 @@ const generateFilm = (index = 0) => {
     age: `12+`,
     isFavorite: getRandomBoolean(),
     isWatched: getRandomBoolean(),
-    watchingDate: getRandomDate(new Date(`1980-01-01`)),
+    watchingDate: getRandomDate(new Date(`2019-11-01`)),
     isWatchlistAdded: getRandomBoolean(),
     director: `John Doe`,
     writers: [`John Doe`, `Vasya Pupkin`],
