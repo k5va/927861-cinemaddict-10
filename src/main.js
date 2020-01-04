ffee787dd20fe8aad4f520a15839fe33e8a7a0d8
k5va <k5va@yandex.ru>
2020-01-04 16:21:30 +0300

    Updated main to use API

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import {UserProfileComponent, FooterStaticticsComponent} from "./components";
-import {generateFilms} from "./mock/film";
 import {render} from "./utils";
 import {MenuController} from "./controllers";
 import {FilmsModel} from "./models";
+import {END_POINT, AUTHORIZATION} from "./consts";
+import API from "./api";
 
-const FILMS_COUNT = 14;
 const filmsModel = new FilmsModel();
-filmsModel.setFilms(generateFilms(FILMS_COUNT));
+const api = new API(END_POINT, AUTHORIZATION);
 
 // render user profile
 const headerElement = document.querySelector(`.header`);
@@ -14,9 +14,15 @@ render(headerElement, new UserProfileComponent(filmsModel.getFilmsAll()));
 
 // render menu
 const mainElement = document.querySelector(`.main`);
-const menuController = new MenuController(mainElement, filmsModel);
-menuController.render();
+const menuController = new MenuController(mainElement, filmsModel, api);
 
 // render footer statistics
 const footerElement = document.querySelector(`.footer`);
 render(footerElement, new FooterStaticticsComponent(filmsModel.getFilmsAll().length));
+
+api
+  .getFilms()
+  .then((films) => {
+    filmsModel.setFilms(films);
+    menuController.render();
+  });
